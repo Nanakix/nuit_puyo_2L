75fd9c694502e95a69140762ceb22eb1fd85a635
Nillyr <nicolas.grellety@gmail.com>
2016-12-01 22:16:36 +0100

    Configure project

---
+++ puyoplayer/cmake-build-debug/CMakeFiles/puyoplayer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/puyoplayer.dir/main.c.o"
+  "puyoplayer.pdb"
+  "puyoplayer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/puyoplayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ puyoplayer/main.c
@@ -0,0 +1,10 @@
+//
+// Created by nillyr on 01/12/16.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  printf("Config Project\n");
+  return 0;
+}
